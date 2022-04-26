a286a97800711f22da69d49e153b8bfcac907c17
ajaychand123 <ajaychandat@gmail.com>
2022-04-26 16:57:55 +0530

    ajay17

---
+++ src/org/git/GitSample.java
@@ -0,0 +1,26 @@
+package org.git;
+
+
+public class GitSample {
+	private void companyName() {
+		System.out.println("Company Name is Comcast");
+
+	}
+	private void companyId() {
+		System.out.println("Company Id is 2410");
+		
+	}
+	private void companyAddress() {
+		System.out.println("Company Address  is chennai");
+	}
+	public static void main(String[] args) {
+		GitSample s = new GitSample();
+		s.companyAddress();
+		s.companyId();
+		s.companyName();
+	}
+
+
+
+
+}
